9fa9735fccb8ccbab46a9a591999ad3670c96ea0
Daniel <heinkokowoko@gmail.com>
2022-07-23 15:21:21 +0200

    [CTA-4] Chat opeartion and animation

--- Chat_App/components/Chat.tsx
+++ Chat_App/components/Chat.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import {View,Text} from 'react-native'
-
+import React, { useState } from 'react';
+import {View,Text, TextInput, Button, FlatList, ListRenderItem} from 'react-native'
+import {ChatItem,RenderChatItem} from './ChatItem'
+import styles from './Styles';
 type ChatProps = {
     username: string,
     image: string;
 };
 
 const Chat = ({username, image}: ChatProps) =>{
+    let[chatInput,setChatInput]=useState("");
+    let[chatItemList,setChatItemList]=useState<ChatItem[]>([])
+    const renderItem: ListRenderItem<ChatItem> = ({item})=>(
+        <RenderChatItem chatItem={item} username={username}></RenderChatItem>
+    )
     return(
-        <View style={{marginTop: 200, alignSelf: 'center'}}>
-            <Text>Hi {username}! You can chat here shortly</Text>
+        <View style={styles.container}>
+            <FlatList
+            inverted
+            data={chatItemList.sort((a,b)=>b.timeStamp- a.timeStamp)}
+            keyExtractor={(item)=>item.id}
+            renderItem={renderItem}
+            ></FlatList>
+            <View style={styles.sendSection}>
+            <TextInput
+                style={styles.chatTextInput}
+                value={chatInput}
+                onChangeText={(text: React.SetStateAction<string>)=>setChatInput(text)}
+            />
+            <Button
+            title="Send"
+            onPress={()=>{
+                setChatItemList([
+                    ...chatItemList,
+                    {
+                        id:Math.random().toString(36).substring(7),
+                        text:chatInput,
+                        image:image,
+                        timeStamp:Date.now(),
+                        by:username,
+                    },
+                ]);
+                setChatInput("");
+            }}
+            />
+        </View>
         </View>
     );
 }
